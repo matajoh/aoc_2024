4290221ae629ab753a21e9ead11e75e40919ed0b
Matthew Johnson <matthew@matthewajohnson.org>
2024-12-01 15:25:02 +0000

    Day 01
    Signed-off-by: Matthew Johnson <matthew@matthewajohnson.org>

--- day01.fs
+++ day01.fs
@@ -3,25 +3,38 @@ module Day01
 open System
 open System.IO
 
-let part1 (values : string seq) =
-    values
-    |> Seq.map to_digits
-    |> Seq.map calibration_value
-    |> Seq.sum 
+let splitAndConvert (s: String) =
+    let parts = s.Split("   ")
+    parts[0] |> int, parts[1] |> int
 
+let part1 lhs rhs =
+    List.zip (List.sort lhs) (List.sort rhs)
+    |> List.map (fun (a, b) -> abs a - b)
+    |> List.sum
 
-let part2 (values : string list) =
-    values 
-    |> Seq.map (replaceText 0)
-    |> part1
+let countInstances counts x =
+    match Map.tryFind x counts with
+    | Some(count) -> Map.add x (count + 1) counts
+    | None -> Map.add x 1 counts
+
+let similarityScore counts x =
+    match Map.tryFind x counts with
+    | Some(count) -> x * count
+    | None -> 0
+
+let part2 lhs rhs =
+    let counts = List.fold countInstances Map.empty rhs
+    lhs |> List.map (similarityScore counts) |> List.sum
 
 let run =
     printfn "== Day 01 =="
-    
-    let values = 
+
+    let lhs, rhs =
         File.ReadLines("inputs/day01.txt")
+        |> Seq.map splitAndConvert
         |> Seq.toList
-    
-    printfn "Part 1: %i" (part1 values)
-    printfn "Part 2: %i" (part2 values)
+        |> List.unzip
+
+    printfn "Part 1: %i" (part1 lhs rhs)
+    printfn "Part 2: %i" (part2 lhs rhs)
     printfn ""
